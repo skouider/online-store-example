27348e4be7db4446b3519e6b7f2f13e8e283993a
funktion911 <kouider.e.s@gmail.com>
2021-12-09 08:11:58 +0100

    firstcommt

---
+++ src/test/java/com/enset/OnlineStoreExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
